5798f119a8e9606cfd3886f86aec42a8c8d6996a
Miguel Romero <miguelromereado@outlook.com>
2020-06-09 22:25:46 -0500

    add revised

--- revised.c
+++ revised.c
@@ -35,7 +35,68 @@
 Declaration of Include files 
 */
 
-#include <stdio.h>   // access terminal
-#include <stdlib.h>  // access system
-#include <stdbool.h> // boolean variables
-#include <string.h>  // string functions
+#include <stdio.h>     // access terminal
+#include <stdlib.h>    // access system
+#include <stdbool.h>   // boolean variables
+#include <string.h>    // string functions
+#include "constants.h" // constants
+
+float res[21][21];                       // (global) ANS as matrix
+float sys[10];                           // (global) ANS as system
+float matrix_a[2][2] = {{1, 2}, {3, 4}}; // (global) matrix A
+float matrix_b[10][10];                  // (global) matriz B
+
+int mn = 0;              // (global) current size of matrix
+bool is_valid_a = false; // (global) matrix is usable
+bool is_valid_b = false; // (global) matrix is usable
+
+int printTitle()
+{
+    printf("\n");
+    printf("\n");
+    printf("\t     ::.----.`    `.---..:////:-.       \n");
+    printf("\t     ymh-:::::-  -:::::-dmmmmmmmmds:    \n");
+    printf("\t     ymm:::::::  `````` .-::::/+ohmmd:  \n");
+    printf("\t     ymm:::::::       `   `-:::::-/hmmo \n");
+    printf("\t     ymm:::::::      .:-    .::::::-hmm/\n");
+    printf("\t     ymm:::::::     -:::-o-  .:::::::mmy\n");
+    printf("\t     ymm:::::::   `-:::-syy-  :::::::mmy\n");
+    printf("\t     ymm:::::::  `:::::yyyy.  :::::::mmy\n");
+    printf("\t     ymm:::::::  .::-/yyyy-   :::::::mmy\n");
+    printf("\t     ymm:::::::`  --/yyys.    :::::::mmy\n");
+    printf("\t     omms-::::::`   :yyo`     :::::::mmy\n");
+    printf("\t     `hmmo-::::::.   .+`      :::::::mmy\n");
+    printf("\t      `smmdo/--:::-.`         :::::::mmy\n");
+    printf("\t        -sdmmdhyyyyyy-::::::  ::::::-dmy\n");
+    printf("\t          `-+osyyyys/-::::-`   .-:::-:oo\n");
+    printf("\n");
+    printf("\t :::::::::::::::::::::::::::::::::::::::::::::\n");
+    printf("\t ::            KEYBOARD MATH '75            ::\n");
+    printf("\t :::::::::::::::::::::::::::::::::::::::::::::\n");
+    printf("\n");
+    printf("\n");
+    system("pause");
+    return 0;
+}
+
+int main()
+{
+    printTitle();
+    arrayTranspose();
+}
+
+int arrayTranspose()
+{
+    for (int i = 0; i < mn; ++i)
+        for (int j = 0; j < mn; ++j)
+            res[j][i] = matrix_a[i][j];
+
+    //1 2    1 3
+    //3 4    2 4
+
+    //print array
+    for (int i = 0; i < mn; ++i)
+        for (int j = 0; j < mn; ++j)
+            printf("%.2f", res[j][i]);
+    return 0;
+}
